fd11a7368d109312a97879dbf46a966166b03aed
Marusya <mspletneva_1@edu.hse.ru>
2023-07-05 16:06:05 +0500

    initial

---
+++ ImoneyService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MONEY.Contracts
+{
+
+    [ServiceContract(Namespace = "https://cbr.ru/DailyInfoWebServ/DailyInfo.asmx")]
+
+    public interface ImoneyService
+    {
+        [OperationContract]
+        double FahrenheitToCelsius(double farenheitDegrees);
+
+        [OperationContract]
+        double CelsiusToFahrenheit(double celsiusDegrees);
+    }
+
+}
+
---
+++ moneyService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MONEY
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "moneyService" в коде, SVC-файле и файле конфигурации.
+    // ПРИМЕЧАНИЕ. Чтобы запустить клиент проверки WCF для тестирования службы, выберите элементы moneyService.svc или moneyService.svc.cs в обозревателе решений и начните отладку.
+    public class moneyService : ImoneyService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
